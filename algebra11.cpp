54c57cc23bf6fced1804e5f465fa84cfe306bc2c
humyoo <143996292+humyoo@users.noreply.github.com>
2023-09-07 22:55:14 +0300

    Add files via upload

---
+++ algebra11.cpp
@@ -0,0 +1,15 @@
+﻿
+#include <iostream>
+#include <math.h>
+
+int main()
+{
+     int a = 1;
+     float b = 20.01;
+     double result = 0, M_PI = 3.14159;
+
+     result = pow((0.719 / b) * ((b * b - a * a) / (a * a + b * b)) + cos(b * log(M_PI / 6)), 1.0 / 3);
+     std::cout << "Result: " << result;
+       
+ 
+}
