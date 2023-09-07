4660994ec4da0f9dd535ba03db6a7566ec99ea71
humyoo <143996292+humyoo@users.noreply.github.com>
2023-09-08 00:13:34 +0300

    Add files via upload

---
+++ algebra16.cpp
@@ -0,0 +1,20 @@
+﻿
+
+#include <iostream>
+#include <math.h>
+
+int main()
+{
+   int a = 3;
+   float b = 0.501;
+   double M_PI = 3.14159;
+
+   double numerator = pow(((a - b), 2) * sin(pow(1 - a / b, 2 * M_PI / 3)) * cos(pow(1 - b / a, 2* M_PI / 3)), 1. / 3);; //чисельник
+   double denominator = 0.701 * log( pow(a - b, 2)) ;  //знаменник
+
+   double result =  numerator / denominator; 
+
+  std::cout << "Result: " << result;
+   
+}
+
