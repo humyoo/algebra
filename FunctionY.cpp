ec101474e6187b5472828efba0be3007086041f9
humyoo <143996292+humyoo@users.noreply.github.com>
2023-09-17 20:11:43 +0300

    Add files via upload

---
+++ FunctionY.cpp
@@ -0,0 +1,30 @@
+﻿
+
+#include <iostream>
+#include <string>
+#include <cmath>
+
+int main()
+{
+    int x, a, b, c;
+    double w;
+
+    std::cout << "Enter x value: ";
+    std::cin >> x;
+    std::cout << "Enter a value: ";
+    std::cin >> a;
+    std::cout << "Enter b value: ";
+    std::cin >> b;
+    std::cout << "Enter c value: ";
+    std::cin >> c;
+
+    if (b>5)
+    {
+        w = pow(4 - x, a * b);
+    }
+    else if (b >= c)
+    {
+        w = (1. / 4 + b * c);
+    }
+    std::cout << "Result: " << w << std::endl;
+}
---
+++ Rectangle1.cpp
@@ -0,0 +1,24 @@
+﻿
+#include <string>
+#include <iostream>
+
+int main()
+{
+    int x, y;
+
+    std::cout << "Enter x value: ";
+    std::cin >> x;
+    std::cout << "Enter y value: ";
+    std::cin >> y;
+
+    if (x >= 0 && x <= 2 && y >= 0 && y <= 2)
+    {
+        std::cout << "WIN";
+    }
+    else
+    {
+        std::cout << "LOSE";
+    }
+}
+
+
