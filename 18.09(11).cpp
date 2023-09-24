304643c7700dd7b86c166e98df106c05807f9403
humyoo <143996292+humyoo@users.noreply.github.com>
2023-09-24 21:16:29 +0300

    Add files via upload

---
+++ 18.09(11).cpp
@@ -0,0 +1,36 @@
+﻿
+#include <iostream>
+
+int main() {
+    int N;
+    std::cout << "Enter a three-digit positive number N: ";
+    std::cin >> N;
+
+    if (N >= 100 && N <= 999) { 
+        int digit1 = N / 100; 
+        int digit2 = (N / 10) % 10; 
+        int digit3 = N % 10; 
+
+        int count = 0; 
+
+        if (digit1 != 1 && digit1 != 9) {
+            count++;
+        }
+
+        if (digit2 != 1 && digit2 != 9) {
+            count++;
+        }
+
+        if (digit3 != 1 && digit3 != 9) {
+            count++;
+        }
+
+        std::cout << "Numbers that are not equal to 1 and 9: " << count << std::endl;
+    }
+    else {
+        std::cout << "Enter a valid three-digit positive number." << std::endl;
+    }
+
+    return 0;
+}
+
---
+++ busstation.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+
+int main() {
+    int busNumber;
+
+    std::cout << "Enter the bus number from the following: 198, 757, 825, 280, 195, 303, 777. " << std::endl;
+    std::cout << "Your choice: ";
+    std::cin >> busNumber;
+
+    int stops;
+
+    switch (busNumber) {
+    case 198:
+        stops = 10;
+        break;
+    case 757:
+        stops = 15;
+        break;
+    case 825:
+        stops = 12;
+        break;
+    case 280:
+        stops = 20;
+        break;
+    case 195:
+        stops = 8;
+        break;
+    case 303:
+        stops = 25;
+        break;
+    case 777:
+        stops = 18;
+        break;
+    default:
+        std::cout << "There is no information about the bus with this number." << std::endl;
+        return 1; 
+    }
+
+    std::cout << "Number of bus stops " << busNumber << ": " << stops << std::endl;
+
+    return 0;
+}
+
